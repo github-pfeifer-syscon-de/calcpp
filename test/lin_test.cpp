af05084095107d2e48ced1816702c57c9c6b3ceb
Roland Pfeifer <RolandPf67@googlemail.com>
2025-11-08 15:16:26 +0100

    calcpp, add test

--- test/lin_test.cpp
+++ test/lin_test.cpp
@@ -29,11 +29,11 @@
 static constexpr auto VALUE_LIMIT{0.000001};
 
 static bool
-check_gaus()
+check_gauss()
 {
     try {
-        const int rows{3};
-        psc::mat::MatrixU<double> m{static_cast<size_t>(rows)};
+        const size_t rows{3u};
+        psc::mat::MatrixU<double> m(rows);
         //row,col
         m[0][0] = 2.0;   m[0][1] = 1.0;  m[0][2] = -1.0;  m[0][3] = 8.0;
         m[1][0] = -3.0;  m[1][1] = -1.0; m[1][2] = 2.0;   m[1][3] = -11.0;
@@ -81,9 +81,46 @@ check_quad()
 }
 
 static double
-random(std::mt19937& rng)
+random(std::mt19937& rng, bool allowNegative = true)
 {
-    return static_cast<double>(rng() % 10000u) / static_cast<double>(rng() % 1000u + 1u);
+    auto n = static_cast<int32_t>(rng() % 10000u);
+    if (allowNegative && (rng() % 3) == 0) {
+        n = -n;
+    }
+    auto d = static_cast<int32_t>(rng() % 1000u + 1u);
+    //std::cout << "n " << n << " d " << d << " n/d "<< (static_cast<double>(n) / static_cast<double>(d)) << std::endl;
+    return static_cast<double>(n) / static_cast<double>(d);
+}
+
+static bool
+check_gauss_rng(size_t cnt)
+{
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    const size_t rows{3u};
+    psc::mat::MatrixU<double> m(rows);
+    for (size_t i = 0; i < cnt; ++i) {
+        auto x1 = random(rng);
+        auto x2 = random(rng);
+        auto x3 = random(rng);
+        m[0][0] = random(rng);   m[0][1] = random(rng);  m[0][2] = random(rng);
+        m[0][3] = (x1 * m[0][0] + x2 * m[0][1] + x3 * m[0][2]);
+        m[1][0] = random(rng);   m[1][1] = random(rng);  m[1][2] = random(rng);
+        m[1][3] = (x1 * m[1][0] + x2 * m[1][1] + x3 * m[1][2]);
+        m[2][0] = random(rng);   m[2][1] = random(rng);  m[2][2] = random(rng);
+        m[2][3] = (x1 * m[2][0] + x2 * m[2][1] + x3 * m[2][2]);
+        psc::mat::Gauss::eliminate(m);
+        if (std::abs(m[0][rows] - x1) > VALUE_LIMIT
+         || std::abs(m[1][rows] - x2) > VALUE_LIMIT
+         || std::abs(m[2][rows] - x3) > VALUE_LIMIT) {
+            std::cout << "x1 exp " << x1 << " got " << m[0][rows]
+                      << "  x2 exp " << x2 << " got " << m[1][rows]
+                      << "  x3 exp " << x3 << " got " << m[2][rows]
+                      << std::endl;
+            return false;
+        }
+    }
+    return true;
 }
 
 static bool
@@ -92,7 +129,6 @@ check_quad_rng(size_t cnt)
     std::random_device dev;
     std::mt19937 rng(dev());
     psc::math::QuadraticEquation<double> quad;
-
     for (size_t i = 0; i < cnt; ++i) {
         auto a = random(rng);
         auto b = random(rng);
@@ -101,23 +137,28 @@ check_quad_rng(size_t cnt)
         quad.setA(a);
         quad.setB(b);
         quad.setC(c);
-        if (std::abs(quad.getX1() - x) > VALUE_LIMIT) {
+        bool isX1 = std::abs(quad.getX1() - x) <= VALUE_LIMIT;
+        bool isX2 = std::abs(quad.getX2() - x) <= VALUE_LIMIT;
+        if (!isX1 && !isX2) {
             std::cout << "not expected x for a " << a
                       << " b " << b
                       << " c " << c
                       << " x " << x
-                      << " x1 " << quad.getX1() << std::endl;
+                      << " x1 " << quad.getX1()
+                      << " x2 " << quad.getX2()  << std::endl;
             return false;
         }
-        auto x2 = quad.getX2();
-        auto tc = -(a * x2 * x2 + b * x2);
+        auto otherX = isX1 ? quad.getX2() : quad.getX1();
+        auto tc = -(a * otherX * otherX + b * otherX);
         if (std::abs(tc - c) > VALUE_LIMIT) {
             std::cout << "not expected c for a " << a
                       << " b " << b
                       << " c " << c
                       << " x " << x
-                      << " tc " << tc
-                      << " x2 " << x2 << std::endl;
+                      << " useX " << otherX
+                      << " x1 " << quad.getX1()
+                      << " x2 " << quad.getX2()
+                      << " tc " << tc << std::endl;
             return false;
         }
     }
@@ -131,14 +172,17 @@ int main(int argc, char** argv)
     setlocale(LC_ALL, "");      // make locale dependent, and make glib accept u8 const !!!
     Glib::init();
 
-    if (!check_gaus()) {
+    if (!check_gauss()) {
         return 1;
     }
     if (!check_quad()) {
         return 2;
     }
-    if (!check_quad_rng(1000)) {
+    if (!check_quad_rng(1000u)) {
         return 3;
+    }
+    if (!check_gauss_rng(1000u)) {
+        return 4;
     }
 
     return 0;
